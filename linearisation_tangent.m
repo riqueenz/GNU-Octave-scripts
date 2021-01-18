a67a4e43e1cfa79ad212a47ab0cb06f971ebfeeb
riqueenz <henriqueenzweiler@gmail.com>
2021-01-19 00:13:44 +0100

    Update linearisation_tangent.m

--- linearisation_tangent.m
+++ linearisation_tangent.m
@@ -5,7 +5,7 @@ clear
 clc
 syms I0z theta_pp k theta F L x1 x2 x h
 
-disp("Modele non lineaire")
+disp('Modele non lineaire')
 eqn = I0z*theta_pp+k*(L^2)*sin(x1)*cos(x1) == F*(L/2)*cos(x1)
 
 theta_pp = expand(solve(eqn, theta_pp));
@@ -13,7 +13,7 @@ theta_pp = expand(solve(eqn, theta_pp));
 f1 = x2
 f2 = theta_pp
 
-disp("Modele linearise tangent au point d'equilibre")
+disp('Modele linearise tangent au point d'equilibre')
 Jx = jacobian([f1, f2], [x1 x2])
 Ju = jacobian([f1, f2],[F])
 
@@ -26,7 +26,7 @@ I0z = 50*10^3 # kg*m^2
 A = subs(Jx)
 B = subs(Ju)
 
-disp("Comandabilite do systeme")
+disp('Comandabilite do systeme')
 p(h)=det(h*eye(2)-A)
 solve(p)
 Ccom = [B, A*B]
