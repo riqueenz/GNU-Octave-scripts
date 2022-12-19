1905ab2a7277b98ee7f7c037ed935f31a4993939
Henrique Enzweiler <henriqueenzweiler@gmail.com>
2022-12-19 11:33:45 -0300

    Update exo1.m

--- S7_Motores de combustão interna/01_Preparação Prova 1/exo1.m
+++ S7_Motores de combustão interna/01_Preparação Prova 1/exo1.m
@@ -1,7 +1,10 @@
 clear
 pkg load symbolic
 
-syms A D pi S Vdu
+# Declarando variaveis simbolicas
+syms A D pi S Vdu rv z Vd V1 V2
+
+# Inicio da resolução
 A = (pi * D * D) / 4
 Vdu = A * S
 
@@ -12,5 +15,14 @@ S = 7.8 #cm
 disp("Podemos encontrar Vdu:")
 A = (pi * D * D) / 4;
 Vdu = A * S
-disp("Vdu em cm3:")
+disp("Cilidrada de um pistão - Vdu em cm3:")
 double(Vdu)
+
+disp("\nSabendo que:")
+z = 4 #cilindros
+Vd = Vdu * z
+disp("Cilidrada - Vd em cm3:")
+double(Vd)
+
+disp("\nSabendo que rv = (Vdu / 2) + 1")
+rv = (Vdu / V2) + 1
