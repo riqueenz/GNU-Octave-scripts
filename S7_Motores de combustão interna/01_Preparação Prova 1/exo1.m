9bdbad14d9fd2683a82e21a69a1ec1dd42f5f5b6
Henrique Enzweiler <henriqueenzweiler@gmail.com>
2022-12-19 13:38:02 -0300

    Resolução exo1

--- S7_Motores de combustão interna/01_Preparação Prova 1/exo1.m
+++ S7_Motores de combustão interna/01_Preparação Prova 1/exo1.m
@@ -11,6 +11,8 @@ Vdu = A * S
 disp("Sabendo que:")
 D = 8.2 #cm
 S = 7.8 #cm
+z = 4 #cilindros
+rv = 8.5
 
 disp("Podemos encontrar Vdu:")
 A = (pi * D * D) / 4;
@@ -18,11 +20,19 @@ Vdu = A * S
 disp("Cilidrada de um pistão - Vdu em cm3:")
 double(Vdu)
 
-disp("\nSabendo que:")
-z = 4 #cilindros
+disp("\nSabendo que Vd = Vdu * z:")
 Vd = Vdu * z
 disp("Cilidrada - Vd em cm3:")
 double(Vd)
 
-disp("\nSabendo que rv = (Vdu / 2) + 1")
-rv = (Vdu / V2) + 1
+disp("\nSabendo que rv = (Vdu / V2) + 1")
+disp("Então V2 = Vdu / (rv - 1)")
+V2 = Vdu / (rv - 1)
+disp("Volume morto - V2 em cm3:")
+double(V2)
+
+disp("\nSabendo que rv = V1 / V2")
+disp("Então V1 = rv * V2")
+V1 = rv * V2
+disp("Volume total de 1 cilindro - V1 em cm3:")
+double(V1)
