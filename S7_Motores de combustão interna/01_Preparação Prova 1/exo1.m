209e660d9407381227e1558ab342818f26ac5c65
Henrique Enzweiler <henriqueenzweiler@gmail.com>
2022-12-19 15:02:01 -0300

    Mudanças nos exos

--- S7_Motores de combustão interna/01_Preparação Prova 1/exo1.m
+++ S7_Motores de combustão interna/01_Preparação Prova 1/exo1.m
@@ -16,29 +16,29 @@ rv = 8.5
 
 # Passando tudo para simbolico para evitar mensagem chata
 # E melhorar precisão do cálculo
-D = sym (D, 'f');
-S = sym (S, 'f');
-rv = sym (rv, 'f');
+D = sym (D, 'r');
+S = sym (S, 'r');
+rv = sym (rv, 'r');
 
 disp("\nPodemos encontrar Vdu = A * S:")
 A = (pi * D * D) / 4;
-Vdu = A * S;
+Vdu = A * S
 disp("Cilidrada de um pistão - Vdu em cm3:")
 double(Vdu)
 
 disp("\nSabendo que Vd = Vdu * z:")
-Vd = Vdu * z;
+Vd = Vdu * z
 disp("Cilidrada - Vd em cm3:")
 double(Vd)
 
 disp("\nSabendo que rv = (Vdu / V2) + 1")
 disp("Então V2 = Vdu / (rv - 1)")
-V2 = Vdu / (rv - 1);
+V2 = Vdu / (rv - 1)
 disp("Volume morto - V2 em cm3:")
 double(V2)
 
 disp("\nSabendo que rv = V1 / V2")
 disp("Então V1 = rv * V2")
-V1 = rv * V2;
+V1 = rv * V2
 disp("Volume total de 1 cilindro - V1 em cm3:")
 double(V1)
