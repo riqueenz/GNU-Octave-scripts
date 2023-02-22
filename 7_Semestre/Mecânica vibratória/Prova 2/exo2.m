e73ffcb84f524a00a5557915ad67a839f5352963
Henrique Enzweiler <henriqueenzweiler@gmail.com>
2023-02-22 14:45:42 -0300

    Create exo2.m

---
+++ 7_Semestre/Mecânica vibratória/Prova 2/exo2.m
@@ -0,0 +1,16 @@
+clear
+clc
+pkg load symbolic
+syms wn wd k m fn c Cc epsilon sigma Td
+
+m = 20 # Kg
+x0 = 0.01 # m
+x1 = 0.004993 # m
+Td = 0.06 # m
+
+sigma = log(x0/x1)
+epsilon = sigma/sqrt((4*(pi**2))+sigma**2)
+wd = (2*pi)/Td
+wn = wd/(1-(epsilon**2))
+k = m*(wn**2)
+c = 2*m*wn*epsilon
